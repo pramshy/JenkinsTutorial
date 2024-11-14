97e7630239ccdb1c7c864ecb9855e2ac0af97f54
pramshy <prameet.impetus@gmail.com>
2024-11-14 21:39:50 +0530

    Adding Sparse checkout steps

--- jenkins-course/job-dsl/nodejs.groovy
+++ jenkins-course/job-dsl/nodejs.groovy
@@ -1,12 +1,12 @@
 job('NodeJS example') {
     scm {
         git('https://github.com/pramshy/JenkinsTutorial.git') / 'extensions' / 'hudson.plugins.git.extensions.impl.SparseCheckoutPaths' / 'sparseCheckoutPaths' {
-            ['docker-demo'].each {
 
-                node -> // is hudson.plugins.git.GitSCM
-                    node / gitConfigName('DSL User')
-                    node / gitConfigEmail('jenkins-dsl@newtech.academy')
+             node -> // is hudson.plugins.git.GitSCM
+                node / gitConfigName('DSL User')
+                node / gitConfigEmail('jenkins-dsl@newtech.academy')
 
+                ['docker-demo'].each {
                 mypath ->
                 'hudson.plugins.git.extensions.impl.SparseCheckoutPath' {
                     path("${mypath}")
